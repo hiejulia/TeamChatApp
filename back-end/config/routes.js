e8bfe06e16514fd8731c5e961189afddd66c717a
Hien Nguyen <hienminhnguyen711@gmail.com>
2017-04-08 09:12:18 +0300

    commit route

--- back-end/config/routes.js
+++ back-end/config/routes.js
@@ -1,1 +1,18 @@
 'use strict';
+
+
+
+
+function initRoutes(app) {
+  const routesPath = app.get('root') + '/app/routes';
+
+  app.use('/auth', require(routesPath + '/auth'));
+  app.use('/api', require(routesPath + '/users'));
+  app.use('/api', require(routesPath + '/chat'));
+  app.use('/api', require(routesPath + '/thread'));
+}
+//auth = auth
+
+//========= users + chat + thread = api
+
+module.exports.init = initRoutes;
