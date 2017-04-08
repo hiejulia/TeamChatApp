7ebc73fd3909c0dace02beb1d2e0bef53571024d
Hien Nguyen <hienminhnguyen711@gmail.com>
2017-04-08 09:06:48 +0300

    mongoose config

--- back-end/config/mongoose.js
+++ back-end/config/mongoose.js
@@ -1,1 +1,37 @@
 'use strict';
+var mongoose = require('mongoose');
+var config = require('./index');
+
+function initMongoose(app) {
+    //connect to mongodb
+     mongoose.connect(config.mongodb.uri);
+     // If the Node process ends, cleanup existing connections
+  process.on('SIGINT', cleanup);
+  process.on('SIGTERM', cleanup);
+  process.on('SIGHUP', cleanup);
+
+  if (app) {
+      //set var
+    app.set('mongoose', mongoose);
+  }
+
+  return mongoose;
+
+
+}
+function cleanup(){
+    //close mongo db connection
+    mongoose.connection.close(() => {
+    process.exit(0);
+  });
+}
+
+
+
+
+
+
+
+
+//exports
+module.exports.init = initMongoose;
