e0def203bf4e439d04d2dd96ea1b970083784d06
Hien Nguyen <hienminhnguyen711@gmail.com>
2017-04-08 09:22:10 +0300

    create helper for password

--- back-end/app/models/user.js
+++ back-end/app/models/user.js
@@ -1,2 +1,13 @@
 'use strict';
 
+const _ = require('lodash');
+const mongoose = require('mongoose');
+const passwordHelper = require('../helpers/password');
+const Schema = mongoose.Schema;
+const Mixed = Schema.Types.Mixed;//schema for mixed types
+
+
+const UserSchema = new Schema({
+
+
+});
