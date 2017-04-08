d72cc2eac4c5fee0a0dd49529e51ecdb704021bd
Hien Nguyen <hienminhnguyen711@gmail.com>
2017-04-08 09:04:47 +0300

    main model file

--- back-end/config/models.js
+++ back-end/config/models.js
@@ -1,1 +1,13 @@
 'use strict';
+
+
+module.exports.init = initModels;
+
+function initModels(app) {
+  const modelsPath = app.get('root') + '/app/models/';
+//user model
+//thread,message
+  ['user', 'thread', 'message'].forEach(function(model) {
+    require(modelsPath + model);
+  });
+};
